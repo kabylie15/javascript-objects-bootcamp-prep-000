c6ecc7560acfb9e66e69cd85cde232bdd76cfde1
Malik Bouzid <malik.amazigh78@gmail.com>
2019-07-30 01:03:57 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  artist: "oatmeal",
+  lunch: "turkey sandwich",
+  dinner: "steak and potatoes"
+};
+
+meals.breakfast = ["oatmeal", "banana"];
+ 
+meals;
+
+Object.assign({}, meals, { breakfast: ['oatmeal', 'banana'] })
